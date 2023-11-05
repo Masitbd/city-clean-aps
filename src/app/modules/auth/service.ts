bc73921a3551752035ff0730601951814ae0bbc8
sarker24 <sarker24@gmail.com>
2023-11-05 20:24:44 +0100

    Create token and transform password

--- src/app/modules/auth/service.ts
+++ src/app/modules/auth/service.ts
@@ -1,5 +1,41 @@
-const signUp = () => {
+import httpStatus from 'http-status';
+import { Secret } from 'jsonwebtoken';
+import config from '../../../config';
+import ApiError from '../../../errors/ApiError';
+import { jwtHelpers } from './../../../helpers/jwtHelpers';
+import { ISignInPayload, IUser } from './interface';
+import { User } from './modal';
+import { AuthUtils } from './utils';
 
-}
+const signUp = async (payload: IUser) => {
+  payload.password = await AuthUtils.hashPass(payload.password);
+  const user = await User.create(payload);
+  return user;
+};
 
-export const AuthService = {signUp}
+const signIn = async (payload: ISignInPayload) => {
+  const { email, password } = payload;
+  const userExist = await User.findOne({ email }).lean();
+
+  if (!userExist) {
+    throw new ApiError(httpStatus.NOT_FOUND, 'User account not found');
+  }
+
+  if (!(await AuthUtils.passMatched(password, userExist.password))) {
+    throw new ApiError(httpStatus.BAD_REQUEST, 'User credentials is wrong');
+  }
+
+  // Generate token
+
+  const tokenPayload = { _id: userExist._id, role: userExist.role };
+
+  const accessToken = jwtHelpers.generateToken(
+    tokenPayload,
+    config.jwt.secret as Secret,
+    config.jwt.expires_in as string
+  );
+  //console.log('hello user',userExist);
+  return { ...userExist, accessToken };
+};
+
+export const AuthService = { signUp, signIn };
---
+++ src/app/modules/auth/utils.ts
@@ -0,0 +1,17 @@
+import bcrypt from 'bcrypt';
+import config from '../../../config';
+
+const hashPass = async (password: string) => {
+  const hashedPass = await bcrypt.hash(
+    password,
+    Number(config.bycrypt_salt_rounds)
+  );
+  return hashedPass;
+};
+
+const passMatched = async (password: string, hashPass: string) => {
+  const hashedPass = await bcrypt.compare(password, hashPass);
+  return hashedPass;
+};
+
+export const AuthUtils = { hashPass, passMatched };
