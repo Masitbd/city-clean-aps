deb01b1f2fb8a96f4debb27b062b1a8489930981
sarker24 <sarker24@gmail.com>
2023-11-12 20:46:37 +0100

    Add booking module

---
+++ src/app/modules/booking/routes.ts
@@ -0,0 +1,36 @@
+import express from 'express';
+import { ENUM_USER_ROLE } from '../../../enums/user';
+import auth from '../../middlewares/auth';
+import validateRequest from '../../middlewares/validateRequest';
+import { BookingController } from './controller';
+import { BookingValidations } from './validation';
+
+const router = express.Router();
+
+router.post(
+  '/',
+  auth(ENUM_USER_ROLE.CUSTOMER),
+  validateRequest(BookingValidations.createBooking),
+  BookingController.createAdmin
+);
+
+router.get(
+  '/',
+  auth(ENUM_USER_ROLE.SUPER_ADMIN, ENUM_USER_ROLE.ADMIN),
+  BookingController.getBookings
+);
+
+router.get(
+  '/:id',
+  auth(ENUM_USER_ROLE.SUPER_ADMIN, ENUM_USER_ROLE.ADMIN),
+  BookingController.getBooking
+);
+
+router.patch(
+  '/update-status/:id',
+  auth(ENUM_USER_ROLE.SUPER_ADMIN, ENUM_USER_ROLE.ADMIN),
+  validateRequest(BookingValidations.updateBookingStatus),
+  BookingController.updateBookingStatus
+);
+
+export const BookingRoutes = router;
